61c62763cb16a6b091f8be75e6d702d38d2e03ec
yasamansh <Yasaman.corona@gmail.com>
2018-02-17 23:03:22 -0800

    Initial commit

---
+++ Home.js
@@ -0,0 +1,39 @@
+﻿var app = angular.module("githubviewer", []);
+app.controller('apiCall', function ($scope, github, $interval, $log, $anchorScroll, $location) {
+
+    $scope.search = function (username) {
+        $log.info("Searching for " + username);
+        github.getUser(username).then(function success(data) {
+                 if (countdownInterval) {
+                     $interval.cancel(countdownInterval);
+                     $scope.countdown = null;
+                 }
+                 $scope.user = data;
+                 github.getRepos($scope.user).then(function success(data) {
+                         $scope.repos = data;
+                         $location.hash("userDetails");
+                         $ancherScroll();
+                     }, function error(response) {
+                         $scope.message = "Could not get the Repos information";
+                     })
+             }, function error(response) {
+                 $scope.message = "Could not fetch the data";
+             });
+         $scope.repoSortOrder = "-stargazers_count";
+    }
+
+     var decrementCountdown = function () {
+         $scope.countdown -= 1;
+         if ($scope.countdown < 1) {
+             $scope.search($scope.username);
+         }
+     };
+     var countdownInterval = null;
+     var startCountdown = function () {
+         countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+     };
+
+     $scope.username = "angular";
+     $scope.countdown = 5;
+     startCountdown();
+});
---
+++ github.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    var github = function ($http) {
+
+        var getUser = function (username) {
+            return $http.get("http://api.github.com/users/" + username)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+        var getRepos = function (user) {
+            return $http.get(user.repos_url)
+                .then(function (response) {
+                    return response.data
+                });
+        };
+        return {
+            getUser: getUser,
+            getRepos: getRepos
+        };
+    }
+
+    var module = angular.module("githubviewer");
+    module.factory("github", github)
+
+}());
